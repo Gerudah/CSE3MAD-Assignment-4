76d12721fad854a42a7f23e45ccfa9d0432927fa
Gerudah <62789547+Gerudah@users.noreply.github.com>
2026-04-15 21:06:50 +1000

    Fixed a bug with the status bar where it wasn't changing with light/dark mode. This was because StatusBar was not part of the apps rootstack
    it is now passed through the app's root with a styling option depending on the state of isDark, which is the value used to determeine dark mode themeing.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,27 @@
 import { ThemeProvider, useAppTheme } from '@/constants/ContextTheme';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 function RootStack() {
-  const { theme } = useAppTheme();
+  const { theme, isDark } = useAppTheme();
 
   return (
-    <Stack
-      screenOptions={{
-        headerTitleAlign: 'center',
-        headerStyle: { backgroundColor: theme.colors.surface },
-        headerTintColor: theme.colors.onSurface,
-        headerTitleStyle: { color: theme.colors.onSurface },
-      }}
-    >
-      <Stack.Screen name="login" options={{ title: 'Login' }} />
-      <Stack.Screen name="team-formation" options={{ title: 'Team Formation' }} />
-      <Stack.Screen name="rating" options={{ title: 'Rating' }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar style={isDark ? 'light' : 'dark'} />
+      <Stack
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: { backgroundColor: theme.colors.surface },
+          headerTintColor: theme.colors.onSurface,
+          headerTitleStyle: { color: theme.colors.onSurface },
+        }}
+      >
+        <Stack.Screen name="login" options={{ title: 'Login' }} />
+        <Stack.Screen name="team-formation" options={{ title: 'Team Formation' }} />
+        <Stack.Screen name="rating" options={{ title: 'Rating' }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
 
